58f8cef207f65be8e0bec1f785eb5cd5b3629582
Omkar Dusane <omkardusane2@hotmail.com>
2017-03-14 21:05:07 +0000

    changes

--- sampleGetRequest/index.js
+++ sampleGetRequest/index.js
@@ -1,35 +1,7 @@
 module.exports = function (context, req) {
     let dogs = 100;
-    let commons = require('../modules/commons.js');
-    //context.log(commons.incr());
     res = {
-        body: {ok:true,message:'done!', cats:commons.incr(), dogs: ++dogs}
+        body: {ok:true,message:'Enjoy the information', dogs: ++dogs}
     };
     context.done(null, res);    
 };
-
-/**
- * Responses from the calls
- * 1.   {
-          "ok": true,
-          "message": "done!",
-          "cats": 101,
-          "dogs": 101
-        }
- * 
- * 2.   {
-          "ok": true,
-          "message": "done!",
-          "cats": 102,
-          "dogs": 101
-        }
- * 
- * 3.   {
-          "ok": true,
-          "message": "done!",
-          "cats": 103,
-          "dogs": 101
-        }
- *
- *  
- */
